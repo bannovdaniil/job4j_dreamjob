f0a34aa92f7e76a3abd0e23d36fd71e48779550a
bannovdaniil <baddan@mail.ru>
2024-01-24 01:25:16 +0300

    1. Страница login.html [#504863]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -24,6 +24,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model) {
+        var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        return "redirect:/vacancies";
+    }
+
     @GetMapping("/register")
     public String getRegistationPage() {
         return "users/register";
